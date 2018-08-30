437e4c4018d70c329e90fdd336af061a7ac7f27f
Johannes Weiß <johannes.weiss@iteratec.de>
2018-08-30 21:27:16 +0200

    Some improvements to the timer

--- scripts/stoppuhr.js
+++ scripts/stoppuhr.js
@@ -1,15 +1,21 @@
+let stoppuhr;
+
 function stoppuhrStarten(rundenende) {
-  const x = setInterval(function () {
-    const verbleibendeSekunden = Math.round((Date.parse(rundenende.toString()) - Date.now()) / 1000);
+  clearInterval(stoppuhr);
+  const ende = Date.parse(rundenende.toString());
+
+  stoppuhr = setInterval(function () {
+    const verbleibendeSekunden = Math.round((ende - Date.now()) / 1000);
     stoppuhrAnzeigen(verbleibendeSekunden);
 
     if (verbleibendeSekunden < 0) {
-      clearInterval(x);
+      clearInterval(stoppuhr);
+      stoppuhrAnzeigen(0);
     }
   }, 1000);
 }
 
 function stoppuhrAnzeigen(sekunden) {
-  const stoppuhr = document.querySelector("#stoppuhr-wert");
-  stoppuhr.innerHTML = sekunden;
+  const stoppuhrAnker = document.querySelector("#stoppuhr-wert");
+  stoppuhrAnker.innerHTML = sekunden;
 }
