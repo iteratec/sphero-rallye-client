c13d93d1e55f6b1d70efb8968b6ff7275ce523f5
Johannes Weiß <johannes.weiss@iteratec.de>
2018-08-31 12:31:37 +0200

    Added help for timer

--- scripts/stoppuhr.js
+++ scripts/stoppuhr.js
@@ -1,3 +1,13 @@
+//
+//
+// Die Funktion "stoppuhrStarten" wird in der Datei "spielBeitreten.js" benutzt um die Stoppuhr zu starten
+// wenn eine Nachricht in dem Thema "spheroRallye/roundEnd" geschrieben wird.
+//
+// Damit die Stoppuhr startet muss sie hier erst einmal programmiert werden.
+// Dazu sollt ihr zwei Funktionen programmieren. Eine Funktion die die Stoppuhr startet und eine Funktion
+// die die Stoppuhr auf der Webseite anzeigt.
+
+
 let stoppuhr;
 
 function stoppuhrStarten(rundenende) {
@@ -14,6 +24,22 @@ function stoppuhrStarten(rundenende) {
     }
   }, 1000);
 }
+
+
+
+
+// Damit der Wert der verbleibenden Sekunden auf der Webseite angezeigt werden,
+// müsst ihr diese in das richtige Element schreiben.
+// Ein Element auf der Webseite könnt ihr auswählen, indem ihr schreibt:
+//
+//   const element = document.querySelector("#identifikator");
+//
+// wenn das Element die Beschreibung id="identifikator" hat.
+//
+// Um Text auf die Webseite zu schreiben, kann man folgendes nutzen:
+//
+//   element.innerHTML = "Dieser Text soll auf die Webseite"
+
 
 function stoppuhrAnzeigen(sekunden) {
   const stoppuhrAnker = document.querySelector("#stoppuhr-wert");
